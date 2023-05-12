59c25d39a9b428f37d96f9da85514ac2fdd33d54
Mark <mark.pijnenburg@hu.nl>
2023-05-12 16:35:06 +0200

    started with making a minimax solution

--- moves.py
+++ moves.py
@@ -1,4 +1,15 @@
+'''
+This script contains the various functions that
+code the moves of the AI-player.
 
+The most simple move-function is that the AI-player
+performs a random move. This is encoded in 
+AI_move_random
+
+A more advanced AI-player will for example use a
+mini-,ax algorithm. This is coded in the function
+AI_move_minimax  
+'''
 import numpy as np
 
 def AI_move_random(position: np.ndarray, player : str) -> np.ndarray:
@@ -15,6 +26,9 @@ def AI_move_random(position: np.ndarray, player : str) -> np.ndarray:
     new_board[row, move] = 1 if player == 'player 1' else 2
     return new_board
 
+def AI_move_minimax(position: np.ndarray, player : str) -> np.ndarray:
+    pass
+    
 if __name__ == '__main__':
     position = np.zeros((6, 7))
     position[5,0] = 1
